d12eaf6cd6a341c3aff2cd51850eff7f5421ac21
dufferbrady <92andrew.brady@gmail.com>
2020-06-24 15:37:01 +1000

    updated net blance using gloabl state

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { GlobalContext } from "../../context/GlobalState";
 
 export default function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.value);
+
+  const total = amounts.reduce((acc, item) => acc + item);
+
+  const sign = total > 0 ? "+" : "-";
+  const color = total > 0 ? "#54c22b" : "#ff5a52";
+
   return (
     <>
       <div>Net Balance</div>
-      <div style={{ fontSize: "40px" }}>
-        <strong>€0.00</strong>
+      <div style={{ fontSize: "40px", color: `${color}` }}>
+        <strong>
+          {sign}
+          {total}
+        </strong>
       </div>
     </>
   );
--- src/components/Breakdown/Breakdown.js
+++ src/components/Breakdown/Breakdown.js
@@ -8,13 +8,13 @@ export default function Breakdown() {
       <div className="breakdown-blockA">
         <span>Income</span>
         <span style={{ fontSize: "27.5px", color: "#54C22B" }}>
-          <strong>+€0.00</strong>
+          <strong>€0.00</strong>
         </span>
       </div>
       <div className="breakdown-blockB">
         <span>Expense</span>
         <span style={{ fontSize: "27.5px", color: "#FF5A52" }}>
-          <strong>-€0.00</strong>
+          <strong>€0.00</strong>
         </span>
       </div>
     </div>
