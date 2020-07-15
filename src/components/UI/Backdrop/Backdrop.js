e5ef773c7691954d5d52f0bfbbe541c82bfc69cf
dufferbrady <92andrew.brady@gmail.com>
2020-07-15 12:16:08 +1000

    initial commit for backend

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "../../../App.css";
-
-export default function Backdrop({ show, click }) {
-  return (
-    <div>{show ? <div className="Backdrop" onClick={click}></div> : null}</div>
-  );
-}
